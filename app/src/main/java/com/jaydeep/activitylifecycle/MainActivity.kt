4799195f569ce5b8507599abaee5e105ccf09af8
Jaydeep <jaysgaikwad123@gmail.com>
2026-01-04 16:02:56 +0530

    Learnt LinearLayout and XML tags

---
+++ app/src/main/java/com/jaydeep/activitylifecycle/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.jaydeep.activitylifecycle
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jaydeep.activitylifecycle.ui.theme.ActivityLifecycleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        println("Oncreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("Onstart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("OnStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("OnRestart")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        println("OnDestroy")
+    }
+
+
+}
+
