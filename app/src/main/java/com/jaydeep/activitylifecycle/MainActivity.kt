90c569818008fecd91803ef9972d95dbde85cfb8
Jaydeep <jaysgaikwad123@gmail.com>
2026-01-04 16:18:50 +0530

    Learnt RelativeLayout

--- app/src/main/java/com/jaydeep/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/jaydeep/activitylifecycle/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_relative_layout)
         println("Oncreate")
     }
 
